22cb8a6d36e5def39a0d0583f62678397229f1d7
Sabana-Yasmine <sabana.platosys@gmail.com>
2022-08-31 19:30:44 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react"
+import "./App.css";
+import {BrowserRouter as Router, Routes, Route} from  "react-router-dom"
+import Home  from './component/Home';
+import About from './component/About';
+import Signin from './component/Signin';
+import Header from './component/Header';
+import Pagenotfound from './component/pagenotfound';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Header />
+      <Routes>
+      <Route path = "/" element = {<Home/>}/>
+        <Route path = "/home" element = {<Home/>}/>
+        <Route path = "/about" element = {<About/>}/>
+        <Route path = "/Signin" element = {<Signin/>}/>
+        <Route path = "*" element = {<Pagenotfound/>}/>
+      </Routes>
+    </Router>
+    </>
   );
 }
 
---
+++ src/component/Signin.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Signin(props) {
+    return (
+        <div>
+            <h3>this is signin page</h3>
+        </div>
+    );
+}
+
+export default Signin;
